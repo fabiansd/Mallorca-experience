db456eab14fe532aff80afb486f3273ba141948e
fabiansd <fabiansd1402@gmail.com>
2020-09-30 09:50:21 +0200

    Theme setup

---
+++ mallorca-klient/src/styles/Theme.tsx
@@ -0,0 +1,16 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import 'fontsource-roboto';
+
+const theme = createMuiTheme({
+    palette: {
+        // type: 'dark',
+        primary: {
+            main: '#3b3b40'
+        },
+        secondary: {
+            main: '#f69e48'
+        }
+    }
+});
+
+export default theme;
